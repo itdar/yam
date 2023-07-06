9ab195fa35153af006283ab0afd7dc8aecddcdde
오형진/서버개발팀/ED <gino@nhnedu.com>
2023-07-07 03:23:12 +0900

    first commit

---
+++ src/test/java/com/yam/yam/YamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yam.yam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
